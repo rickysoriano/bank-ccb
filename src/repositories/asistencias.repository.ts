32512e91c4fb4b348494b0f1a04930942a2edfe2
francisco-ag <francisamado80@gmail.com>
2023-08-07 23:31:37 -0600

    back

--- src/repositories/asistencias.repository.ts
+++ src/repositories/asistencias.repository.ts
@@ -2,6 +2,7 @@ import {inject} from '@loopback/core';
 import {DefaultCrudRepository} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
 import {Asistencias, AsistenciasRelations} from '../models';
+import {error} from 'console';
 
 export class AsistenciasRepository extends DefaultCrudRepository<
   Asistencias,
@@ -13,4 +14,26 @@ export class AsistenciasRepository extends DefaultCrudRepository<
   ) {
     super(Asistencias, dataSource);
   }
+
+  //asistencia : any []
+  //id :number, fecha:string
+  registrarAsistencia(id :number, fecha:string, hora:string){
+    let sql : string = `select * from ccb_registrar_asistencia(
+      '${id}',
+      '${fecha}',
+      '${hora}'
+    )`
+    // '${asistencia.id_alumno}',
+    //   '${asistencia.fecha}'
+    // '${id}',
+    // '${fecha}',
+
+    return new Promise((resolve, reject)=>{
+      this.dataSource.execute(sql)
+      .then(result => resolve(result))
+      .catch(error => reject(error));
+    })
+
+  }
+
 }
